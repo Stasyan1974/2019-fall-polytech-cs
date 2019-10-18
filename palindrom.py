fcc3ff481719ea048b79400e97dfc61391784a4d
Stasyan1974 <55135260+Stasyan1974@users.noreply.github.com>
2019-10-19 00:22:26 +0300

    Update palindrom.py

--- palindrom.py
+++ palindrom.py
@@ -7,6 +7,6 @@ def pal(x):
 def pal(x):
   x = x.lower()
   return x == "".join(reversed(x)):
-if __name__ == __main__:
+if __name__ == __main__:       # ?
   assert pal("LEveL") == True
   assert pal("IFNIT") == False
