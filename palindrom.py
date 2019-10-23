12aae8e673fadd3c119b039885b09e2a6add3509
Stasyan1974 <55135260+Stasyan1974@users.noreply.github.com>
2019-10-23 03:30:51 +0300

    Update palindrom.py

--- palindrom.py
+++ palindrom.py
@@ -1,8 +1,13 @@
-def pal(x):
-  x = x.lower()
-  return x == "".join(reversed(x)):
-
+def pal(data1):
+  data1 = data1.lower()
+  data2 = reversed(data1)
+  for x, y in zip(data1, data2):
+    if x == y:
+      print("yes")
+    else:
+      print("no")
+      
 # Тест
-if __name__ == __main__:       # ?
-  assert pal("LEveL") == True
-  assert pal("IFNIT") == False
+if __name__ == __main__:       
+  assert pal("AbbA") == "yes\nyes\nyes\nyes"
+  assert pal("aReA") == "yes\nno\nno\nyes"
