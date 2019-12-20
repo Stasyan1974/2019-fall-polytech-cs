e84fbe220cc9fa622ff43e8d5c40fc266f5685e9
Stasyan1974 <55135260+Stasyan1974@users.noreply.github.com>
2019-12-20 22:52:25 +0300

    Update 414.py

--- 414.py
+++ 414.py
@@ -1,25 +1,19 @@
-def lus(s):
+def MaxSubstring(s):
   i = 1
-  uniq = 1
+  counter = 1
   res = 1
   end = len(s) - 1
   while i <= end:
     if s[i] != s[i-1]:
-      uniq += 1
+      counter += 1
     else:
-      uniq = 1
-    if res < uniq:
-      res = uniq
+      counter = 1
+    if res < counter:
+      res = counter
     i += 1
   return res
-  
-def test_lus(s, res, testId):
-  if lus(s) == res:
-    print("Test " + str(testId) + " complete")
     
  # ТЕСТ
- s1 = "abcdeeeeefghhhhhijklmnop"
- test_lus(s1, 9, 1))
- 
- s2 = "1234567890"
- test_lus(s2, 10, 2)
+if __name__ == __main__: 
+  assert (MaxSubstring("abcdddddertffffxxxotfewqvn") == 9), "программа работает неправильно"
+  assert (MaxSubstring("12345678999abcc") != 9), "программа работает правильно"
